fc598078d4e27ebc80bad94f3f7dd8a370e99668
XtraordinaryXTD <xtraordinary132@gmail.com>
2022-12-22 15:37:33 +0100

    data bases

--- app/routes.py
+++ app/routes.py
@@ -32,8 +32,7 @@ def feed():
 def login():
     form = LoginForum()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('/index'))
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
 
