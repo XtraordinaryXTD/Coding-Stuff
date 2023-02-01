1771957926df3bef8f8835f358ae03094fcfde27
XtraordinaryXTD <xtraordinary132@gmail.com>
2023-02-01 13:31:53 +0100

    using Bootstrap3

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask_migrate import Migrate
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 
 
@@ -18,6 +19,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 
 if not app.debug:
